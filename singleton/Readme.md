abb780d356243afd1433a36a62edc97d37c57a5b
chrisloarryn <chrisloarryn@gmail.com>
2022-09-10 23:39:20 -0300

    feat: add singleton JS/TS

--- singleton/Readme.md
+++ singleton/Readme.md
@@ -1,1 +1,4 @@
 # creacional
+
+### singleton is common in dependency injection, the object is the same
+### across the entire app
--- singleton/js/singleton.js
+++ singleton/js/singleton.js
@@ -1,30 +1,28 @@
-console.log("singleton.js");
+console.log('singleton.js')
 
 class Singleton {
-
-  static getInstance () {
+  static getInstance() {
     if (!Singleton.instance) {
-      Singleton.instance = new Singleton();
+      Singleton.instance = new Singleton()
     }
-    return Singleton.instance;
+    return Singleton.instance
   }
 
   constructor() {
-    console.log("enter to the constructor");
-    this.random = Math.random();
+    console.log('enter to the constructor')
+    this.random = Math.random()
     if (Singleton.instance) {
-      console.log("return existing instance");
-      return Singleton.instance;
+      console.log('return existing instance')
+      return Singleton.instance
     }
-    console.log("if doesn't exist, create a new instance");
-    Singleton.instance = this;
+    console.log("if doesn't exist, create a new instance")
+    Singleton.instance = this
   }
 }
 
 // const firstSingletonInstance = new Singleton();
 // const secondSingletonInstance = new Singleton();
 // const thirdSingletonInstance = new Singleton();
-
 
 // console.log(firstSingletonInstance.random);
 // console.log(secondSingletonInstance.random);
@@ -33,39 +31,52 @@ class Singleton {
 // console.log(firstSingletonInstance === secondSingletonInstance);
 // console.log(secondSingletonInstance === thirdSingletonInstance);
 
-
 // const anotherInstance = Singleton.getInstance();
 
 // console.log(anotherInstance.random);
 
 class WeekDays {
-  dayES = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
-  dayEN = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  dayES = [
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado',
+    'Domingo',
+  ]
+  dayEN = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ]
 
   constructor(lang) {
-    if (!["es", "en"].includes(lang)) {
-      throw new Error("Language not supported");
+    if (!['es', 'en'].includes(lang)) {
+      throw new Error('Language not supported')
     }
 
-    this.lang = lang;
-    if(WeekDays.instance) {
-      return WeekDays.instance;
+    this.lang = lang
+    if (WeekDays.instance) {
+      return WeekDays.instance
     }
-    WeekDays.instance = this;
+    WeekDays.instance = this
   }
 
-  getDays () {
-    return this.lang === "es" ? this.dayES : this.dayEN;
+  getDays() {
+    return this.lang === 'es' ? this.dayES : this.dayEN
   }
-
-
 }
 
-const weekDaysES = new WeekDays("es");
-const weekDaysEN = new WeekDays("en");
+const weekDaysES = new WeekDays('es')
+const weekDaysEN = new WeekDays('en')
 
 // error because language not supported
 // const weekDaysFR = new WeekDays("fr");
 
-console.log(weekDaysES.getDays());
-console.log(weekDaysEN.getDays());
+console.log(weekDaysES.getDays())
+console.log(weekDaysEN.getDays())
