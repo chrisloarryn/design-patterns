e8444c6ce86055be9fdd0712c009c708e87acf53
chrisloarryn <chrisloarryn@gmail.com>
2022-09-12 02:50:50 -0300

    chore: files moved to another directory

--- solid/clean-course/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>CleanCode y SOLID</h1>
-  <span>Revisar la consola de JavaScript</span>
-`
-
