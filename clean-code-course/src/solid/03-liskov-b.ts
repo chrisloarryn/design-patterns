d10a52830cad95d96c9a7f3f6fe5a8e5d55e807c
Cristobal Andres Contreras Orellana <ext_cristobal.contreras@cajalosandes.cl>
2023-01-01 22:41:42 -0300

    feat: L liskov
    Co-Authored-By: Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>

---
+++ clean-code-course/src/solid/03-liskov-b.ts
@@ -0,0 +1,70 @@
+export abstract class Vehicle {
+		/* getNumberOfSeats(): number {
+			throw new Error('Method not implemented.');
+		} */
+	
+	abstract getNumberOfSeats(): number;
+}
+
+
+export class Tesla extends Vehicle {
+		constructor( private numberOfSeats: number ) {
+			super();
+		}
+
+		getNumberOfSeats() {
+				return this.numberOfSeats;
+		}
+}
+
+export class Audi extends Vehicle {
+	constructor(private numberOfSeats: number) {
+		super()
+	}
+
+	getNumberOfSeats() {
+		return this.numberOfSeats;
+	}
+}
+
+export class Toyota extends Vehicle {
+	constructor(private numberOfSeats: number) {
+		super()
+	}
+
+	getNumberOfSeats() {
+		return this.numberOfSeats;
+	}
+}
+	
+export class Honda extends Vehicle {
+	constructor(private numberOfSeats: number) {
+		super()
+	}
+
+	getNumberOfSeats() {
+		return this.numberOfSeats;
+	}
+}
+
+// Volvo
+export class Volvo extends Vehicle {
+	constructor(private numberOfSeats: number) {
+		super()
+	}
+
+	getNumberOfSeats() {
+		return this.numberOfSeats;
+	}
+}
+
+// Ford
+export class Ford extends Vehicle {
+	constructor(private numberOfSeats: number) {
+		super()
+	}
+
+	getNumberOfSeats() {
+		return this.numberOfSeats;
+	}
+}
